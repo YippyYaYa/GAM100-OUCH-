4c1fbd65cdad0b979aed37d3f4d92627f760bb9d
YippyYaYa <yippyyeehaw@hotmail.com>
2018-10-27 01:33:36 +0800

    Revert "Revert "Grid.c changes, new file""
    This reverts commit f3481b9958992080b80aa5e62a31272daf6011e1.

--- Ouch/Project1/Grid.c
+++ Ouch/Project1/Grid.c
@@ -1,24 +1,37 @@
 #include "Grid.h"
 #include <stdio.h>
 #include "WindowsHelper.h"
-#define N 100
+#define N 10
 
 int i, j;
-char grid[N][N] = { '0' };
+FILE*textart;				/*create a variable to store file*/
+char grid[N][N] = { '0' };	/*define a 2D array by size NxN*/
+char name[] = "test.txt";	/*take in file name, needs more work*/
 
-/*Store grid, will upgrade to file i/o*/
+/*Stores grid, need switch/ifelse statements for file level variable intake(name will be hardcoded)*/
 void Grid_initGrid()
 {
+	
+	/*open file for reading*/
+	fopen_s(&textart, name, "r");
+	/*Transverse 2D array*/
 	for(i = 0; i < N; i++)
 	{
-		for(j = 0; j < N; j++)
-			grid[i][j] = '#';
+		for (j = 0; j < N; j++)
+		{
+			/*stores char in file to grid and checks if char in file is newline, if true, re-scan to grid*/
+			if ((grid[i][j] = getc(textart)) == '\n')
+				j--;
+		}
 	}
+	/*close file*/
+	fclose(textart);
 }
 
 /*Prints the grid*/
 void Grid_printGrid()
 {
+	/*Set cursor position to 0,0*/
 	WindowsHelper_SetCursorPosition(0, 0);
 	for (i = 0; i < N; i++)
 	{
