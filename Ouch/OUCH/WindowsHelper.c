59acd41f7eb8de4442e6a00e9de5fcb8f73a6904
yifangchong <yifangchong@gmail.com>
2018-11-15 18:37:07 +0800

    console size again

--- Ouch/OUCH/WindowsHelper.c
+++ Ouch/OUCH/WindowsHelper.c
@@ -5,15 +5,15 @@
 HANDLE wHnd;    /* Handle to write to the console.*/
 HANDLE rHnd;    /* Handle to read from the console.*/
 
-static short width = 100; /* width of console size*/
-static short height = 25; /* height of console size*/
+static short width = 101; /* width of console size*/
+static short height = 26; /* height of console size*/
 
 void WindowsHelper_Init()
 {
 	COORD bufferSize = { width , height };
 	SMALL_RECT windowSize = { 0,0, width - 1, height - 1};
 	CONSOLE_CURSOR_INFO cursorInfo;
-	/*DWORD consoleMode;*/
+	DWORD consoleMode;
 
 	wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
 	rHnd = GetStdHandle(STD_INPUT_HANDLE);
@@ -29,10 +29,10 @@ void WindowsHelper_Init()
 	SetConsoleCursorInfo(wHnd, &cursorInfo);
 
 	/* Disable extra row printing at the end of screen */
-	/*
+	
 	GetConsoleMode(wHnd, &consoleMode);
 	consoleMode |= DISABLE_NEWLINE_AUTO_RETURN | ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-	SetConsoleMode(wHnd, consoleMode);*/
+	SetConsoleMode(wHnd, consoleMode);
 }
 
 /*Sets the console cursor position. Note that (0,0) is the top left of the console*/
