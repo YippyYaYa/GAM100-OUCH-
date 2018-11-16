e70fa30bd3e8c779a1ea6596a9c51a8ad77bbf6c
YippyYaYa <yippyyeehaw@hotmail.com>
2018-11-16 15:02:01 +0800

    SwitchPortal
    Updated Switch to Switch Portal

--- Ouch/OUCH/WindowsHelper.c
+++ Ouch/OUCH/WindowsHelper.c
@@ -5,13 +5,12 @@
 HANDLE wHnd;    /* Handle to write to the console.*/
 HANDLE rHnd;    /* Handle to read from the console.*/
 
-static short width = 101; /* width of console size*/
-static short height = 26; /* height of console size*/
+static short width = 100; /* width of console size*/
+static short height = 25; /* height of console size*/
 
 void WindowsHelper_Init()
 {
 	COORD bufferSize = { width , height };
-	SMALL_RECT windowSize = { 0,0, width , height };
 	CONSOLE_CURSOR_INFO cursorInfo;
 	DWORD consoleMode;
 
@@ -30,10 +29,10 @@ void WindowsHelper_Init()
 
 	/* Disable extra row printing at the end of screen */
 	
-	GetConsoleMode(wHnd, &consoleMode);
+	/*GetConsoleMode(wHnd, &consoleMode);
 	consoleMode |= DISABLE_NEWLINE_AUTO_RETURN | ENABLE_VIRTUAL_TERMINAL_PROCESSING;
 	if(!SetConsoleMode(wHnd, consoleMode))
-		ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
+		ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);*/
 }
 
 /*Sets the console cursor position. Note that (0,0) is the top left of the console*/
