9c4b2ccb2815e877b7f0daaaae9b5ab3cc135e8d
jxisconfused <44506722+jxisconfused@users.noreply.github.com>
2018-12-07 14:16:30 +0800

    comments

--- Ouch/OUCH/Loading.c
+++ Ouch/OUCH/Loading.c
@@ -1,3 +1,14 @@
+/******************************************************************************/
+/*!
+\file   Loading.c
+\author Toh Gim Hsiang
+\par    Course: GAM100
+\par    Copyright © 2018 DigiPen (Singapore) Corporation.
+\brief
+Loading.c is in charge of loading digipen and team logo with colors.
+*/
+/******************************************************************************/
+
 #pragma once
 #include "Loading.h"
 #include "GameStateManager.h"
@@ -10,24 +21,31 @@ static float tPass;
 static int count = 0;
 void Loading_Init()
 {
+	/* reset color */
 	Colours_ResetColor();
+	/* clear screen */
 	system("cls");
+	/* call grid_initgrid to case 6 */
 	Grid_initGrid(6);
+	/* call to print grid*/
 	Grid_printGrid();
 }
 
 void Loading_Update(float dt)
 {
-	/* do stuff here */
+	/* time passes over time with delta time */
 	tPass = tPass + dt;
-	/* Change state*/
-	if(count == 0)               
+
+	if(count == 0)
+    /* if time passed 4 seconds, change state */
 	if (tPass > 4.0f)            
 	{
 		count++;
+		/* call for next printgrid */
 		Grid_initGrid(7);
 		Grid_printGrid();
 	}
+	/* after time passed 7 secs.. change state ot main menu */
 	if(tPass > 7.0f) 
 	GameStateManager_SetGameState(MainMenu);
 }
--- Ouch/OUCH/Loading.h
+++ Ouch/OUCH/Loading.h
@@ -1,3 +1,15 @@
+/******************************************************************************/
+/*!
+\file   Loading.h
+\author Toh Gim Hsiang
+\par    Course: GAM100
+\par    Copyright © 2018 DigiPen (Singapore) Corporation.
+\brief
+This file contains the function declarations for Loading.c
+*/
+/******************************************************************************/
+
+
 #pragma once
 
 void Loading_Init();
